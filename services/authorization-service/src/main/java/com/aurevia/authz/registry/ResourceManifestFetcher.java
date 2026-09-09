021e6ac616ee12dee3c723b18a59817d449a22e9
Mahdi Porkar <mahdiporkar@gmail.com>
2026-09-09 17:01:15 +0330

    Update ResourceManifestFetcher.java
    bug fix :manifest

--- services/authorization-service/src/main/java/com/aurevia/authz/registry/ResourceManifestFetcher.java
+++ services/authorization-service/src/main/java/com/aurevia/authz/registry/ResourceManifestFetcher.java
@@ -1,4 +1,7 @@
 package com.aurevia.authz.registry;
 
 /** Resource-manifest port retained for source compatibility with the resource workflow. */
-public interface ResourceManifestFetcher extends ManifestFetcher {}
+// public interface ResourceManifestFetcher extends ManifestFetcher {}
+public interface ResourceManifestFetcher {
+  String fetch(String sourceUrl);
+}
