890e40137c1aae1ca357aae85ab6315493ab62e2
Mahdi Porkar <mahdiporkar@gmail.com>
2026-09-12 14:30:09 +0330

    Update ProductionOpenFgaConfigurationGuardTest.java
    bug fix

--- services/authorization-service/src/test/java/com/aurevia/authz/openfga/ProductionOpenFgaConfigurationGuardTest.java
+++ services/authorization-service/src/test/java/com/aurevia/authz/openfga/ProductionOpenFgaConfigurationGuardTest.java
@@ -2,21 +2,82 @@ package com.aurevia.authz.openfga;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 class ProductionOpenFgaConfigurationGuardTest {
+
   @Test
   void rejectsMissingOrBootstrapIdentifiers() {
-    assertThrows(IllegalStateException.class,
-        () -> ProductionOpenFgaConfigurationGuard.requireProductionValue("OPENFGA_STORE_ID", ""));
-    assertThrows(IllegalStateException.class,
-        () -> ProductionOpenFgaConfigurationGuard.requireProductionValue(
-            "OPENFGA_MODEL_ID", "created-by-model-deployment"));
+
+    assertThrows(
+        IllegalStateException.class,
+        () ->
+            ProductionOpenFgaConfigurationGuard
+                .requireProductionValue(
+                    "OPENFGA_STORE_ID",
+                    ""
+                )
+    );
+
+    assertThrows(
+        IllegalStateException.class,
+        () ->
+            ProductionOpenFgaConfigurationGuard
+                .requireProductionValue(
+                    "OPENFGA_MODEL_ID",
+                    "created-by-model-deployment"
+                )
+    );
   }
 
   @Test
   void acceptsAnExplicitPinnedIdentifier() {
-    assertDoesNotThrow(() -> ProductionOpenFgaConfigurationGuard.requireProductionValue(
-        "OPENFGA_MODEL_ID", "01M17FCAYW1S6Z347FDR025TND"));
+
+    assertDoesNotThrow(() ->
+        ProductionOpenFgaConfigurationGuard
+            .requireProductionValue(
+                "OPENFGA_MODEL_ID",
+                "01M17FCAYW1S6Z347FDR025TND"
+            )
+    );
+  }
+
+  @Test
+  void rejectsDestructiveStartupRepairWithoutExplicitProductionOptIn() {
+
+    assertThrows(
+        IllegalStateException.class,
+        () ->
+            ProductionOpenFgaConfigurationGuard
+                .requireSafeStartupReconciliation(
+                    true,
+                    false
+                )
+    );
+  }
+
+  @Test
+  void acceptsNormalProductionStartupWithoutReconciliation() {
+
+    assertDoesNotThrow(() ->
+        ProductionOpenFgaConfigurationGuard
+            .requireSafeStartupReconciliation(
+                false,
+                false
+            )
+    );
+  }
+
+  @Test
+  void acceptsExplicitlyApprovedProductionStartupRepair() {
+
+    assertDoesNotThrow(() ->
+        ProductionOpenFgaConfigurationGuard
+            .requireSafeStartupReconciliation(
+                true,
+                true
+            )
+    );
   }
 }
